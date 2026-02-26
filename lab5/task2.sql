36326f3a53af775edf347efd2382ab5434193632
Oyatillo Axadjonov <o.axadjonov@UCA214-145s-iMac.local>
2026-02-26 14:45:35 +0500

    feat: task2.1 done

--- lab5/task2.sql
+++ lab5/task2.sql
@@ -1,3 +1,16 @@
+DROP FUNCTION IF EXISTS get_employee_exp;
+
+-- Task 2.1
+
 CREATE FUNCTION IF NOT EXISTS get_employee_exp(hire_date DATE)
 RETURNS INT
-RETURN YEAR(hire_date) - YEAR(CURDATE());
+DETERMINISTIC
+RETURN TIMESTAMPDIFF(YEAR, hire_date, CURDATE()) + 1;
+
+SELECT
+    name,
+    get_employee_exp(hire_date) AS experience,
+    YEAR(hire_date) AS hired_in,
+    YEAR(CURDATE()) AS current_year
+FROM
+    employees;
