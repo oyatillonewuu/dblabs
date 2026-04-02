058c4df1cde5c7962002121df61027e60d371160
oyatillonewuu <o.axadjonov@newuu.uz>
2026-04-02 15:55:09 +0500

    feat: task1.b done

--- lab8/task1/queries.sql
+++ lab8/task1/queries.sql
@@ -7,3 +7,21 @@ WHERE
     app_type = 'WEB'
     AND
     user_role = 'admin';
+
+- T1.b
+
+SELECT style_config
+FROM tag_styles
+WHERE
+    is_active
+    AND
+    tag_name = 'error_badge'
+ORDER BY
+    priority DESC;
+
+- T1.c
+
+SELECT tag_name
+FROM tag_styles
+WHERE
+    (SELECT FROM JSON_CONTAINTS_PATH(style_config, "$.border-radius")) = 1;
