f024c7ba864ab34cbe88e675de4c5aa788305f97
oyatillonewuu <o.axadjonov@newuu.uz>
2026-04-05 09:42:48 +0500

    feat: fix spatial location definition to comply with SRID requirement

--- lab8/task2/data/restaurants_spatial.sql
+++ lab8/task2/data/restaurants_spatial.sql
@@ -1,8 +1,8 @@
-INSERT INTO restaurants_spatial (name, location, cuisine) VALUES ('Pizza Plaza', Point(37.7755, -122.418), 'Italian');
-INSERT INTO restaurants_spatial (name, location, cuisine) VALUES ('Taco Truck', Point(37.773, -122.421), 'Mexican');
-INSERT INTO restaurants_spatial (name, location, cuisine) VALUES ('Sushi Spot', Point(37.778, -122.415),'Japanese');
-INSERT INTO restaurants_spatial (name, location, cuisine) VALUES ('Burger Barn', Point(37.77, -122.425), 'American');
-INSERT INTO restaurants_spatial (name, location, cuisine) VALUES ('Curry House', Point(37.776, -122.4205), 'Indian');
-INSERT INTO restaurants_spatial (name, location, cuisine) VALUES ('Salad Stop', Point(37.774, -122.4175), 'Healthy');
-INSERT INTO restaurants_spatial (name, location, cuisine) VALUES ('Coffee Corner', Point(37.7775, -122.422), 'Cafe');
-INSERT INTO restaurants_spatial (name, location, cuisine) VALUES ('Noodle House', Point(37.7715, -122.4235), 'Asian');
+INSERT INTO restaurants_spatial (name, location, cuisine) VALUES ('Pizza Plaza', ST_GeomFromText('POINT(37.7755 -122.418)', 4326), 'Italian');
+INSERT INTO restaurants_spatial (name, location, cuisine) VALUES ('Taco Truck', ST_GeomFromText('POINT(37.773 -122.421)', 4326), 'Mexican');
+INSERT INTO restaurants_spatial (name, location, cuisine) VALUES ('Sushi Spot', ST_GeomFromText('POINT(37.778 -122.415)', 4326), 'Japanese');
+INSERT INTO restaurants_spatial (name, location, cuisine) VALUES ('Burger Barn', ST_GeomFromText('POINT(37.77 -122.425)', 4326), 'American');
+INSERT INTO restaurants_spatial (name, location, cuisine) VALUES ('Curry House', ST_GeomFromText('POINT(37.776 -122.4205)', 4326), 'Indian');
+INSERT INTO restaurants_spatial (name, location, cuisine) VALUES ('Salad Stop', ST_GeomFromText('POINT(37.774 -122.4175)', 4326), 'Healthy');
+INSERT INTO restaurants_spatial (name, location, cuisine) VALUES ('Coffee Corner', ST_GeomFromText('POINT(37.7775 -122.422)', 4326), 'Cafe');
+INSERT INTO restaurants_spatial (name, location, cuisine) VALUES ('Noodle House', ST_GeomFromText('POINT(37.7715 -122.4235)', 4326), 'Asian');
